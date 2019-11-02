c8ee6a05ea6436b600d93f754ecdb366af36ba24
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2019-11-02 21:52:06 +0200

    model: Use sklearn LogisticRegression as an example

--- mlh_challenge/model.py
+++ mlh_challenge/model.py
@@ -1,46 +1,64 @@
 import os
 from pathlib import Path
+import logging
 
 import numpy as np
 import pickle
 from sklearn.base import ClassifierMixin, BaseEstimator
 
+from sklearn.linear_model.logistic import LogisticRegression
+
+logger = logging.getLogger(__name__)
+
 
 class MLHChallengeModel(BaseEstimator, ClassifierMixin):
+    """
+    Model class.
+
+    TODO:
+     You can change whatever you want here and/or completely replace
+     this class. If you do, just make sure the functions in run.py can still
+     work with it.
+    """
 
     def __init__(self, foo=1, bar=2, **kw):
         # TODO:
         #  Initialize your model/parameters here.
         #  You can add arguments as necessary for configuring your model.
-        #  You can use any existing sklean estimators, pytorch models,
-        #  or just implement everything from scratch.
-        self.w_ = None
+        #  You can use and combine any existing sklean estimators, pytorch
+        #  models, etc, or just implement everything from scratch.
+
+        # As a demo, we'll use a simple logistic regression model
+        self.lr_model = LogisticRegression(
+            C=.5, penalty='l1', solver='liblinear'
+        )
 
     def fit(self, X, y, **kwargs):
         # TODO:
         #  Implement your training code here.
 
-        # As a demo, our "trained" model will just have random weights...
-        n, d = X.shape
-        self.w_ = np.random.randn(d) * 0.001
+        # As a demo, we just fit our logistic regression model
+        # and print best the features with some fixed threshold
+        self.lr_model.fit(X, y)
+
+        feat_names = kwargs['feat_names']
+        best_features = self.lr_model.coef_[0, :] > 0.1
+        logger.info(f'best features: {list(feat_names[best_features])}')
 
         return self
 
+    def predict_proba(self, X, **kwargs):
+        # TODO: Implement probability calculation for positive class (1).
+        y_proba = self.lr_model.predict_proba(X)
+        return y_proba[:, 1]  # probability of class 1
+
     def predict(self, X, **kwargs):
-        # TODO: Implement your inference code here
-
-        # As a demo, we'll just use our random weights to calculate a
-        # probability and use a fixed threshold for classification.
-        z = np.dot(X, self.w_)
-        z -= np.max(z)
-        z_exp = np.exp(z)
-        p = z_exp / np.sum(z_exp)
-        y_pred = p > 0.01
-
+        # TODO: Implement your inference code here. Return binary labels.
+        y_pred = self.lr_model.predict(X)
         return y_pred
 
     def fit_predict(self, X, y, **kwargs):
-        return self.fit(X, y).predict(X)
+        return self.fit(X, y, **kwargs).predict(X, **kwargs)
 
     def save_state(self, filepath):
         filepath = Path(filepath)  # in case it's a str
@@ -52,7 +70,7 @@ class MLHChallengeModel(BaseEstimator, ClassifierMixin):
         #  Save any trained parameters you need to load later for inference.
         #  If using pytorch, note that modules have (load_)state_dict
         #  methods that you can use.
-        state_dict = {'w_': self.w_}
+        state_dict = {'lr_model': self.lr_model}
 
         with open(filepath, mode='wb') as f:
             pickle.dump(state_dict, f)
@@ -63,4 +81,4 @@ class MLHChallengeModel(BaseEstimator, ClassifierMixin):
 
         # TODO:
         #  Load your saved state into the model.
-        self.w_ = state_dict['w_']
+        self.lr_model = state_dict['lr_model']
