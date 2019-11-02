f9377d970010a5813f505c7626e4bbe398ea8fa7
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2019-11-02 14:57:42 +0200

    data: impute missing samples

--- mlh_challenge/data.py
+++ mlh_challenge/data.py
@@ -2,7 +2,7 @@ import numpy as np
 import logging
 from pathlib import Path
 
-import mlh_challenge
+from sklearn.impute import SimpleImputer
 
 logger = logging.getLogger(__name__)
 
@@ -11,7 +11,7 @@ def load_raw(filename):
     filepath = Path(filename).absolute()
     logger.info(f'Loading data file {filepath}...')
 
-    data = np.load(filepath)
+    data = np.load(filepath, allow_pickle=True)
     return data
 
 
@@ -20,14 +20,21 @@ def build_features(raw_data):
     # TODO:
     #  Process the raw data to build your features for training/inference.
 
-    Xb, Xb_names = raw_data['feat_basic'], raw_data['feat_basic_names']
-    Xp, Xp_names = raw_data['feat_pollution'], raw_data['feat_pollution_names']
+    Xb, Xb_names = raw_data['phys_feat'], raw_data['phys_feat_names']
+    Xp, Xp_names = raw_data['pol_feat'], raw_data['pol_feat_names']
 
-    # As a demo, here we'll just combine all basic and pollution features as
+    # As a demo, here we'll just use all basic and pollution features as
     # they are, without any processing.
-    n_patients, _ = Xb.shape
-    X = np.hstack((Xb, Xp.reshape(n_patients, -1)))
-    feat_names = list(np.hstack((Xb_names, Xp_names)))
+    n_samples, _ = Xb.shape
+    X = np.hstack((Xb, Xp.reshape(n_samples, -1)))
+    Xp_names_flat = [Xp_names[j] + f'_{i + 1:02d}'
+                     for i in range(Xp.shape[1]) for j in range(Xp.shape[2])]
+    feat_names = np.hstack((Xb_names, Xp_names_flat))
+
+    # Handle missing samples: naively replace them with -1 (not a good idea...)
+    imputer = SimpleImputer(missing_values=np.nan, strategy='constant',
+                            fill_value=-1.)
+    X = imputer.fit_transform(X,)
 
     logger.info(f'Processed data shape: {X.shape}')
     return X, feat_names
