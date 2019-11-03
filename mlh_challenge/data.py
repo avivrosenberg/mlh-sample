82e2d9ea58660a32f32007ab4daecd79370bd036
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2019-11-03 23:36:29 +0200

    data: refactor into an estimator
    Split the fitting and the transforming

--- mlh_challenge/data.py
+++ mlh_challenge/data.py
@@ -2,7 +2,8 @@ import numpy as np
 import logging
 from pathlib import Path
 
-from sklearn.impute import SimpleImputer
+from sklearn.base import TransformerMixin, BaseEstimator
+from sklearn.utils.validation import check_is_fitted
 
 logger = logging.getLogger(__name__)
 
@@ -19,52 +20,114 @@ def load_raw(filename):
     """
     Loads raw data in the challenge format.
     :param filename: Path to the data file (should be .npz).
-    :return: A dict-like object with the following keys and values:
-        K_PHYS: 2d tensor (N,D1): N samples of D1 physiological features
-        K_PHYS_NAMES: 1d tensor (D1,): Names of physiological features
-        K_POL: 3d tensor (N,K,D2): N samples of K consecutive days of
+    :return: A tuple with the following elements:
+        Xb: 2d tensor (N,D1): N samples of D1 physiological features
+        Xp: 3d tensor (N,K,D2): N samples of K consecutive days of
             D2 pollution features
-        K_POL_NAMES: 1d tensor (D2,): Names of pollution features
-        K_TARGET: 1d tensor (N,): Binary labels representing severity based
-            on length of stay (label 1 is severe).
+        y: 1d tensor (N,): Binary labels representing severity based
+           on length of stay (label 1 is severe).
+        Xb_names: 1d tensor (D1,): Names of physiological features
+        Xp_names: 1d tensor (D2,): Names of pollution features
     """
     filepath = Path(filename).absolute()
     logger.info(f'Loading data file {filepath}...')
 
-    data = np.load(filepath, allow_pickle=True)
+    raw_data = np.load(filepath, allow_pickle=True)
     for k in ALL_KEYS:
-        if k not in data:
+        if k not in raw_data:
             raise ValueError("Data file does not contain all inputs")
-
-    return dict(**data)
-
-
-def build_features(raw_data):
-    logger.info(f'Processing data...')
-    # TODO:
-    #  Process the raw data to build your features for training/inference.
 
     Xb, Xb_names = raw_data[K_PHYS], raw_data[K_PHYS_NAMES]
     Xp, Xp_names = raw_data[K_POL], raw_data[K_POL_NAMES]
     y = raw_data[K_TARGET]
 
-    # As a demo, here we'll just use all basic and pollution features as
-    # they are, without any processing.
-    n_samples, _ = Xb.shape
-    X = np.hstack((Xb, Xp.reshape(n_samples, -1)))
-    Xp_names_flat = [Xp_names[j] + f'_{i + 1:02d}'
-                     for i in range(Xp.shape[1]) for j in range(Xp.shape[2])]
-    feat_names = np.hstack((Xb_names, Xp_names_flat))
+    return Xb, Xp, y, Xb_names, Xp_names
 
-    # Example: Remove missing features (all-nan columns)
-    missing_features = np.where(np.all(np.isnan(X), axis=0))
-    X = np.delete(X, missing_features, axis=1)
-    feat_names = np.delete(feat_names, missing_features, axis=0)
 
-    # Example: handle missing samples by naively replacing them with feature
-    # median
-    imputer = SimpleImputer(missing_values=np.nan, strategy='median')
-    X = imputer.fit_transform(X, )
+class MLHChallengeFeatureTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, foo=1, bar=2):
+        pass
 
-    logger.info(f'Processed data shape: {X.shape}')
-    return X, y, feat_names
+    def fit(self, Xb, Xp, y=None, Xb_names=None, Xp_names=None, **kw):
+        assert Xb.shape[0] == Xp.shape[0]
+        n_samples, _ = Xb.shape
+
+        # TODO:
+        #  Calculate any necessary metrics about the training data and save
+        #  them as members in this class.
+        #  These should be used to transform the train and test features in
+        #  the same way. For example, use these for feature scaling and
+        #  imputation of missing values.
+
+        # As a demo, here we'll just use all basic and pollution features as
+        # they are, without any processing.
+
+        X = np.hstack((Xb, Xp.reshape(n_samples, -1)))
+
+        # Example 1: Manually removing specific features
+        manually_removed_features = ['x', 'y']
+        manually_removed_idx = []
+        if Xb_names is not None:
+            for name in manually_removed_features:
+                manually_removed_idx.append(np.where(Xb_names == name)[0])
+
+        # Save all removed features as a member
+        self.removed_features_ = np.concatenate(manually_removed_idx, axis=0)
+
+        # Example 2: Calculate feature means and save as a member
+        self.feature_means_ = np.nanmean(X, axis=0)
+
+        return self
+
+    def transform(self, Xb, Xp, Xb_names=None, Xp_names=None, **kw):
+        logger.info(f'Generating features...')
+
+        n_samples, _ = Xb.shape
+        assert Xb.shape[0] == Xp.shape[0]
+
+        # Make sure fit was called and stats were saved
+        check_is_fitted(self, ['removed_features_', 'feature_means_'])
+
+        # TODO:
+        #  Use your saved stats to transform the input. Below are some
+        #  examples using the previously saved stats.
+
+        # Example: treat all pollution sample as separate features
+        X = np.hstack((Xb, Xp.reshape(n_samples, -1)))
+
+        # Example: handle missing samples by replacing them with saved means
+        i_nan, j_nan = np.where(np.isnan(X))
+        X[i_nan, j_nan] = np.take(self.feature_means_, j_nan)
+
+        # Example: Remove features marked for removal
+        X = np.delete(X, self.removed_features_, axis=1)
+
+        # Example: Generate names for pollution features and remove the
+        # names of removed features.
+        feat_names = []
+        if Xb_names is not None and Xp_names is not None:
+            Xp_names_flat = [
+                Xp_names[j] + f'_{i + 1:02d}'
+                for i in range(Xp.shape[1]) for j in range(Xp.shape[2])
+            ]
+            feat_names = np.hstack((Xb_names, Xp_names_flat))
+            feat_names = np.delete(feat_names, self.removed_features_, axis=0)
+
+        logger.info(f'Processed data shape: {X.shape}')
+        return X, feat_names
+
+    def fit_transform(self, Xb, Xp, **kw):
+        return self.fit(Xb, Xp, **kw).transform(Xb, Xp, **kw)
+
+    def save_state(self):
+        # TODO:
+        #  Save members that you need to write to file for later inference.
+        state_dict = {
+            'removed_features_': self.removed_features_,
+            'feature_means_': self.feature_means_
+        }
+
+        return state_dict
+
+    def load_state(self, state_dict):
+        self.__dict__.update(**state_dict)
